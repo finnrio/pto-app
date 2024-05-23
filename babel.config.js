691e4cdb950df218208ff16570583c85473c86bd
finnrio <archiesd@outlook.com>
2024-05-23 20:16:47 +0100

    fix

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,6 @@
-module.exports = function (api) {
+module.exports = function(api) {
   api.cache(true);
   return {
-    presets: [
-      ["@babel/preset-env", { targets: { node: "current" } }],
-      "babel-preset-expo",
-      "@babel/preset-typescript",
-    ],
+    presets: ['babel-preset-expo'],
   };
 };
--- jest.setup.js
+++
@@ -1,8 +0,0 @@
-// include this line for mocking react-native-gesture-handler
-import "react-native-gesture-handler/jestSetup";
-
-const { resolve } = require("path");
-
-require("dotenv").config({
-  path: resolve(__dirname, ".env.test"),
-});
